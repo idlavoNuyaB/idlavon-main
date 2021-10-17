055a4b5a1ecf4f2bf00192f30e0858c0d20c02d2
Bayu Novaldi <36811359+idlavoNuyaB@users.noreply.github.com>
2021-10-17 15:25:02 +0700

    Add card for project

---
+++ src/components/Carousel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="carousel">
+    <div class="card">
+      <div class="img">
+        <img src="../assets/img/android.png">
+      </div>
+      <div class="content">
+        <h2>Title Project</h2>
+        <p class="content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="img">
+        <img src="../assets/img/android.png">
+      </div>
+      <div class="content">
+        <h2>Title Project</h2>
+        <p class="content-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="wrapper" id="wrapper2">
     <h1 id="projectTitle">Project Showcase</h1>
+    <Carousel/>
   </div>
 </template>
+
+<script>
+import Carousel from './Carousel.vue'
+
+export default {
+  name: 'Showcase',
+  components: {
+    Carousel
+  }
+}
+</script>
